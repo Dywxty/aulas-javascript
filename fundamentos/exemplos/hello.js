81a53f3adfe61f51e03ca0467c835a85501bb8ac
Dywxty <leticia.talachia@gmail.com>
2026-02-06 09:24:25 -0300

    Arquivo informação

--- fundamentos/exemplos/hello.js
+++ fundamentos/exemplos/hello.js
@@ -7,3 +7,6 @@ console.log("Hello wolrd Javascript") //print no python
 
 console.log('Vamos aprender a programar em JavaScript') //tanto faz as aspas duplas ou não (' ou ")
 console.log('Tenho' + 16 + 'anos de idade')
+
+// caso der problema no commit/push, user.name e user.email, utilizar o comando no CMD:
+// git config --global user.(name ou email) "email ou name"
---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,18 @@
+//const cria funções - uma constante
+const curso = "Técnico em Desenvolvimento de Sistemas" //string
+const ano = 2025 //inteiro
+const notaMinimaParaSerAprovado = 50 //inteiro
+const notaParaSerReprovado = 49.9 //decimal
+const cursoTecnico = true //constante
+
+console.log("=======================================")
+console.log('==== Minhas informações acadêmicas ====')
+console.log('Curso:' + curso)
+
+console.log(`Ano: ${ano}`) //crase (`) é o template string, ou seja um f no python, pode usar para escrever 
+//e colocar funções dentro
+
+console.log(`Nota para aprovação: ${notaMinimaParaSerAprovado}`)
+console.log(`Nota para reprovação: ${notaParaSerReprovado}`)
+console.log (`É curso técnico: ${cursoTecnico}`)
+console.log("=======================================")
