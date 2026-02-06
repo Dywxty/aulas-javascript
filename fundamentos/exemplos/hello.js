b1ce7195354c0dd2bc5d0ae2640d7c9da15660b1
Dywxty <leticia.talachia@gmail.com>
2026-02-06 08:31:29 -0300

    Arquivo hello

---
+++ fundamentos/exemplos/hello.js
@@ -0,0 +1,9 @@
+console.log("Hello wolrd Javascript") //print no python
+
+//Code runner - C, C++, Java, JS, PHP, Python, Perl, Ruby, Go, Lua, Groovy, PowerShell, CMD,
+//  BASH, F#, C#, VBScript, TypeScript, CoffeeScript, Scala, Swift, Julia, Crystal, OCaml, R, 
+// AppleScript, Elixir, VB.NET, Clojure, Haxe, Obj-C, Rust, Racket, Scheme, AutoHotkey, AutoIt, 
+// Kotlin, Dart, Pascal, Haskell, Nim...
+
+console.log('Vamos aprender a programar em JavaScript') //tanto faz as aspas duplas ou não (' ou ")
+console.log('Tenho' + 16 + 'anos de idade')
